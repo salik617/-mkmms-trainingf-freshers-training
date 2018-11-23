86429a6d2305b16588b683a56dfe34f43b1d7a40
ashok kumar <ashok.b@bitlasoft.com>
2018-11-23 18:37:34 +0530

    creation and deletion done

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,7 +1,7 @@
 function constructUserList(){
 	var listHtml = "";
 
-	listHtml += "<h1>Users</h1>";
+	listHtml += "<h1>Users <button  onclick='createNewUser()' type='button'>create</button> </h1>";
 
 	listHtml += "<table class='table'>";
 
@@ -113,7 +113,7 @@ function editResUser(user){
 	var paramName = document.getElementsByName('csrf-param')[0].content;
 	var token = $("[name='csrf-token']").attr('content');
 
-	html += '<form id ="edit-form" action="/users/'+ user.id +'" accept-charset="UTF-8" method="post" data-remote=true>'
+	html += '<form id ="edit-form" data-type="json" action="/users/'+ user.id +'" accept-charset="UTF-8" method="post" data-remote=true>'
 	html += '<input type="hidden" name="_method" value="patch">'
 	html += '<input type="hidden" name="'+ paramName +'" value="'+ token +'"/>'
 
@@ -175,11 +175,108 @@ function editResUser(user){
 	$('#year').val(dobarray[0]); //jquery
 	$("#myModal").modal('show'); 
 	$("#edit-form").on("ajax:success",function(e,response){
+		debugger
 
 		showResUser(e.detail[0])
 		getUserListContent();
 
 	})
+}
+function createNewUser(){
+	var html="";
+	var paramName = document.getElementsByName('csrf-param')[0].content;
+	var token = $("[name='csrf-token']").attr('content');
+
+	html += '<form id ="create-form" data-type="json" action="/users" accept-charset="UTF-8" method="post" data-remote=true>'
+	html += '<input type="hidden" name="'+ paramName +'" value="'+ token +'"/>'
+
+
+	html += '<p>Name:<input type="text" name="user[name]" /></p>'
+	html += '<p>Email:<input type="email" name="user[email]"/></p>'
+	html += '<p>Phone:<input type="text" name="user[phone]" /></p>'
+	html += '<p>Address:<textarea name="user[address]"></textarea></p>'
+	html += '<p>Date of birth:'
+	html += "<select name='user[dob(3i)]' >"
+	for(var a=1;a<=31;a++)
+	{
+		html += "<option value ='"+ a +"'>"+ a +"</option>"
+	    
+	}
+	html += "</select>"
+
+	html += "<select id='months' name = 'user[dob(2i)]' >"
+	html += "<option value ='01'>01</option>"
+	html += "<option value ='02'>02</option>"
+	html += "<option value ='03'>03</option>"
+	html += "<option value ='04'>04</option>"
+	html += "<option value ='05'>05</option>"
+	html += "<option value ='06'>06</option>"
+	html += "<option value ='07'>07</option>"
+	html += "<option value ='08'>08</option>"
+	html += "<option value ='09'>09</option>"
+	html += "<option value ='10'>10</option>"
+	html += "<option value ='11'>11</option>"
+	html += "<option value ='12'>12</option>"
+	html += "</select>"
+
+	html += "<select id='year'  name='user[dob(1i)]' >"
+	html += "<option value ='1990'>1990</option>"
+	html += "<option value ='1991'>1991</option>"
+	html += "<option value ='1992'>1992</option>"
+	html += "<option value ='1993'>1993</option>"
+	html += "<option value ='1994'>1994</option>"
+	html += "<option value ='1995'>1995</option>"
+	html += "<option value ='1996'>1996</option>"
+	html += "<option value ='1997'>1997</option>"
+	html += "<option value ='1998'>1998</option>"
+	html += "<option value ='1999'>1999</option>"
+	html += "<option value ='2008'>2008</option>"
+	html += "</select>"
+	html += '</p>'
+	html += '<p>Gender:<input type="text" name="user[gender]"/></p>'
+
+	html += '<button type="submit">Submit</button>'
+
+	html += '</form>'
+	$("#user-modal").html( html );
+
+	$("#myModal").modal('show');
+	$("#create-form").on("ajax:success",function(e){
+
+
+      showResUser(e.detail[0]);
+      getUserListContent();
+	}
+	)
+
+
+
+}
+function deleteUser(id){
+	if (confirm("are you sure want to delete?")) {
+		var paramName = document.getElementsByName('csrf-param')[0].content;
+	     var token = $("[name='csrf-token']").attr('content');
+
+		$.ajax({
+			data:{
+				authenticity_token:token
+
+			},
+		url: "/users/" + id,
+		dataType: 'json',
+		method:'delete',
+		success: function(response){
+			getUserListContent();
+		},
+		error: function(){
+
+		}
+	})
+
+
+
+	}
+
 }
 
 //"user"=>{"name"=>"Shivani ", "email"=>"shivisankhla@gmail.com", "phone"=>"7597556898", "address"=>"6th block koramangala", "dob(1i)"=>"2008", "dob(2i)"=>"2", "dob(3i)"=>"2", "gender"=>"Female"}
